c66fd2008fc67d0edcb8dba7a148dd198f85bb74
tctianchi <tctianchi@gmail.com>
2016-11-23 22:53:02 +0800

    单位列表改为由程序生成

--- Civ6Trainer/Frontend/Control/TrainerMenuPanel.xaml.cs
+++ Civ6Trainer/Frontend/Control/TrainerMenuPanel.xaml.cs
@@ -20,6 +20,9 @@ namespace tctianchi.Civ6Trainer.Frontend.Control
     /// </summary>
     public partial class TrainerMenuPanel : UserControl
     {
+        public event EventHandler<EventArgs> RefreshClicked;
+        public event EventHandler<EventArgs> PageSelected;
+        
         public TrainerMenuPanel()
         {
             InitializeComponent();
@@ -27,7 +30,98 @@ namespace tctianchi.Civ6Trainer.Frontend.Control
 
         private void TrainerMenuPanel_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            MessageBox.Show("Refresh");
+            if (RefreshClicked != null)
+            {
+                RefreshClicked.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public enum MenuCategory
+        {
+            Player,
+            City,
+            Army,
+            Science,
+            Debug,
+        }
+
+        public void ClearAll()
+        {
+            playerList.Children.Clear();
+            cityList.Children.Clear();
+            armyList.Children.Clear();
+            scienceList.Children.Clear();
+            debugList.Children.Clear();
+        }
+
+        public TrainerMenuPageSelector AddItem(MenuCategory category, object tag = null, string contentText = "", string bubbleText = "")
+        {
+            TrainerMenuPageSelector item = new TrainerMenuPageSelector();
+            item.IsMarked = false;
+            item.Tag = tag;
+            item.ContentText = contentText;
+            item.BubbleText = bubbleText;
+            item.MouseLeftButtonUp += item_MouseLeftButtonUp;
+            switch (category)
+            {
+                case MenuCategory.Player:
+                    playerList.Children.Add(item);
+                    break;
+                case MenuCategory.City:
+                    cityList.Children.Add(item);
+                    break;
+                case MenuCategory.Army:
+                    armyList.Children.Add(item);
+                    break;
+                case MenuCategory.Science:
+                    scienceList.Children.Add(item);
+                    break;
+                case MenuCategory.Debug:
+                    debugList.Children.Add(item);
+                    break;
+            }
+            return item;
+        }
+
+        private void item_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // 选中元素标记为选中
+            (sender as TrainerMenuPageSelector).IsMarked = true;
+
+            // 其余元素标记为未选中
+            Action<TrainerMenuPageSelector> clearMark = (TrainerMenuPageSelector i) =>
+            {
+                if (i != sender)
+                {
+                    i.IsMarked = false;
+                }
+            };
+            foreach (var i in playerList.Children)
+            {
+                clearMark(i as TrainerMenuPageSelector);
+            }
+            foreach (var i in cityList.Children)
+            {
+                clearMark(i as TrainerMenuPageSelector);
+            }
+            foreach (var i in armyList.Children)
+            {
+                clearMark(i as TrainerMenuPageSelector);
+            }
+            foreach (var i in scienceList.Children)
+            {
+                clearMark(i as TrainerMenuPageSelector);
+            }
+            foreach (var i in debugList.Children)
+            {
+                clearMark(i as TrainerMenuPageSelector);
+            }
+
+            // 触发选中事件
+            if (PageSelected != null)
+            {
+                PageSelected.Invoke(sender, EventArgs.Empty);
+            }
         }
     }
 }
--- Civ6Trainer/Frontend/Trainer/MainWindow.xaml.cs
+++ Civ6Trainer/Frontend/Trainer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using tctianchi.Civ6Trainer.Frontend.Control;
 
 namespace tctianchi.Civ6Trainer.Frontend.Trainer
 {
@@ -20,6 +21,12 @@ namespace tctianchi.Civ6Trainer.Frontend.Trainer
     /// </summary>
     public partial class MainWindow : Window
     {
+        protected class PageTag
+        {
+            public TrainerMenuPanel.MenuCategory Category { get; set; }
+            public object Tag { get; set; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,9 +38,60 @@ namespace tctianchi.Civ6Trainer.Frontend.Trainer
             this.contentFrame.Navigate(new Uri("Frontend/Trainer/" + btn.Tag.ToString() + ".xaml", UriKind.Relative));
         }
 
-        private void TrainerMenuButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void menuPanel_RefreshClicked(object sender, EventArgs e)
         {
-            MessageBox.Show("Refresh");
+            Random rand = new Random();
+
+            menuPanel.ClearAll();
+            menuPanel.AddItem(
+                TrainerMenuPanel.MenuCategory.Player,
+                new PageTag { Tag = "player 0 tag" },
+                "玩家0");
+            menuPanel.AddItem(
+                TrainerMenuPanel.MenuCategory.City,
+                new PageTag { Tag = "city 0 tag" },
+                "城市长长长长长长长长长长长长长长长长",
+                "1");
+            menuPanel.AddItem(
+                TrainerMenuPanel.MenuCategory.City,
+                new PageTag { Tag = "city 1 tag" },
+                $"城市{rand.Next()}",
+                "12");
+            menuPanel.AddItem(
+                TrainerMenuPanel.MenuCategory.City,
+                new PageTag { Tag = "city 2 tag" },
+                $"城市{rand.Next()}",
+                "123");
+            menuPanel.AddItem(
+                TrainerMenuPanel.MenuCategory.City,
+                new PageTag { Tag = "city 3 tag" },
+                "城市长长长长长长长长长长长长长长长长");
+            for (int i = 0; i < 20; i++)
+            {
+                menuPanel.AddItem(
+                    TrainerMenuPanel.MenuCategory.Army,
+                    new PageTag { Tag = $"army {i} tag" },
+                    $"部队{rand.Next()}");
+            }
+            menuPanel.AddItem(
+                TrainerMenuPanel.MenuCategory.Science,
+                new PageTag { Tag = "science 0 tag" },
+                "自然科学");
+            menuPanel.AddItem(
+                TrainerMenuPanel.MenuCategory.Science,
+                new PageTag { Tag = "science 1 tag" },
+                "社会科学");
+            menuPanel.AddItem(
+                TrainerMenuPanel.MenuCategory.Debug,
+                new PageTag { Tag = "debug 0 tag" },
+                "测试1");
+        }
+
+        private void menuPanel_PageSelected(object sender, EventArgs e)
+        {
+            TrainerMenuPageSelector selector = sender as TrainerMenuPageSelector;
+            PageTag tag = selector.Tag as PageTag;
+            MessageBox.Show(tag.Tag as string);
         }
     }
 }
